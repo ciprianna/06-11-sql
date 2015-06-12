0f2d9c57f1ebce16d9c97bf651efd95342f07599
Ciprianna <ciprianna17@gmail.com>
2015-06-11 23:30:15 -0500

    Worked on the methods to add a new event. Not working yet.

--- dinnerclub_sql.rb
+++ dinnerclub_sql.rb
@@ -6,10 +6,10 @@ require_relative "dinnerclub_code_copy.rb"
 DATABASE = SQLite3::Database.new("dinner_clubs.db")
 
 # Creates the table for club_history
-DATABASE.execute("CREATE TABLE IF NOT EXISTS club_history (id INTEGER PRIMARY KEY, date DATE, location TEXT, attendees_and_cost TEXT);")
+DATABASE.execute("CREATE TABLE IF NOT EXISTS club_history (id INTEGER PRIMARY KEY, date TEXT, location TEXT, attendees_and_cost TEXT);")
 
 # Creates the table for running_member_tab
-DATABASE.execute("CREATE TABLE IF NOT EXISTS running_member_tab (name TEXT, total_cost INTEGER)")
+DATABASE.execute("CREATE TABLE IF NOT EXISTS running_member_tab (name TEXT PRIMARY KEY, total_cost INTEGER);")
 
 # Returns the results as a Hash
 DATABASE.results_as_hash = true
@@ -22,4 +22,21 @@ end
 # Reads running_member_tab table
 def show_running_member_tab
   DATABASE.execute("SELECT * FROM running_member_tab;")
+end
+
+# Adds a new event to the club_history table
+def add_club_event(total_cost, tip_percentage, people, date, location, who_paid)
+  new_event = DinnerClub.new
+  new_event.new_event(total_cost, tip_percentage, people, date, location, who_paid)
+  DATABASE.execute("INSERT INTO club_history (date, location) VALUES ('#{date}', '#{location}');")
+  populate_running_member_tab(new_event.running_balance)
+end
+
+# Populates the running_member_tab table when an event is added
+def populate_running_member_tab(running_balance)
+  running_balance.each do |name, cost|
+    DATABASE.execute("INSERT INTO running_member_tab (name) VALUES ('#{name}');")
+    current_total = DATABASE.execute("SELECT total_cost FROM running_member_tab WHERE name = #{name};").first['total_cost']
+    DATABASE.execute("UPDATE running_member_tab SET total_cost = #{total_cost + cost} WHERE name = #{'name'};")
+  end
 end
